6dd015121205d024eb4b50a8bb26b70f8f51fd79
Kariuki Michael <kariukimichael017@gmail.com>
2024-02-26 15:21:05 +0300

    Empty Board Challenge #057

---
+++ Empty-board-057/index.js
@@ -0,0 +1,14 @@
+
+const dropDowns = document.querySelectorAll('.dropdown');
+
+dropDowns.forEach(dropDown => {
+
+    const select = dropDown.querySelector('.select');
+    const menu = dropDown.querySelector('.menu');
+    const caret = dropDown.querySelector('.caret');
+
+    select.addEventListener('click', () => {
+        menu.classList.toggle('menu-active');
+        caret.classList.toggle('caret-rotate');
+    })
+})
