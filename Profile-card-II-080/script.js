74c69a4464c0bed66718b015cabe40fa7bc47ffb
Kariuki Michael <kariukimichael017@gmail.com>
2024-03-20 20:06:02 +0300

    Profile card 2 challenge #080

---
+++ Profile-card-II-080/script.js
@@ -0,0 +1,21 @@
+
+
+// const scrollRevealOption = {
+//     distance: '50px',
+//     origin: 'top',
+//     duration: 1000
+// }
+// scrollReveal().reveal('.profile-card', {
+//     ...scrollRevealOption,
+// })
+ScrollReveal().reveal('.profile-card', {
+    distance: '200px',
+    origin: 'left',
+    duration: 2000
+});
+ScrollReveal().reveal('.profile-settings', {
+    distance: '100px',
+    origin: 'right',
+    duration: 2000,
+    delay: 2000
+});
