fec7e101d3c09ec909aeef2981f36a91e97c3ff7
Kariuki Michael <kariukimichael017@gmail.com>
2024-03-21 17:59:39 +0300

    Profile Card Ui Challenge #081

---
+++ Profile-cards-ui-081/script.js
@@ -0,0 +1,17 @@
+ScrollReveal().reveal('.profile-1', {
+    distance: '200px',
+    origin: 'left',
+    duration: 2000
+});
+ScrollReveal().reveal('.profile-2', {
+    distance: '200px',
+    origin: 'right',
+    duration: 2000,
+    delay: 1000,
+});
+ScrollReveal().reveal('.profile-3', {
+    distance: '200px',
+    origin: 'bottom',
+    duration: 2000,
+    delay: 2000
+});
